4c0a8648313a090c57dcb5b4caff72be7058d6ab
Bruno <brussdr7@gmail.com>
2020-01-25 15:57:52 -0600

    Creando modulos
    Creando modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product.component';
-import { ExponentialPipe } from './exponential.pipe';
+import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
-import { HighlightDirective } from './highlight.directive';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
+
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ExponentialPipe,
     CartComponent,
-    HighlightDirective,
     ProductsComponent,
     ContactComponent,
     DemoComponent,
-    FooterComponent,
-    HeaderComponent,
     PageNotFoundComponent,
     ProductDetailComponent,
     LayoutComponent
@@ -36,7 +30,9 @@ import { LayoutComponent } from './layout/layout.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
