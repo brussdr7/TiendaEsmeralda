e48cee9b4a410a730dc6e4f024044df71391d9da
Bruno <brussdr7@gmail.com>
2020-01-29 10:30:19 -0600

    Footer
    Estilos al Footer

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule, MatBadge} from '@angular/material/badge';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [],
@@ -11,12 +14,16 @@ import {MatIconModule} from '@angular/material/icon';
     CommonModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
