04d385a93f0c1b2a05cc26733bcddf1c87684129
Bruno <brussdr7@gmail.com>
2020-03-05 12:38:24 -0600

    peticion get desde un a Rest API

--- src/app/core/sevices/products/products.service.ts
+++ src/app/core/sevices/products/products.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Product } from './../../models/product.model';
 
 @Injectable({
@@ -6,60 +8,20 @@ import { Product } from './../../models/product.model';
 })
 export class ProductsService {
 
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/bitcoin.jpg',
-      title: 'arroz',
-      price: 10,
-      description: 'x'
-    },
-    {
-      id: '2',
-      image: 'assets/images/gorillaz.jpg',
-      title: 'frijol',
-      price: 5,
-      description: 'x'
-    },
-    {
-      id: '3',
-      image: 'assets/images/responsive.jpg',
-      title: 'maiz',
-      price: 30,
-      description: 'x'
-    },
-    {
-      id: '4',
-      image: 'assets/images/one-more-time.jpg',
-      title: 'fresas',
-      price: 40,
-      description: 'x'
-    },
-    {
-      id: '5',
-      image: 'assets/images/rehuso.jpg',
-      title: 'naranja',
-      price: 10,
-      description: 'x'
-    },
-    {
-      id: '6',
-      image: 'assets/images/html5.jpg',
-      title: 'miel',
-      price: 50,
-      description: 'x'
-    },
-  ];
+  // inyeccion de dependencia
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
-
+  // obtener todos los productos
   getAllProducts() {
-    return this.products;
+    // este get resuelve un array de tipo Product[]
+    return this.http.get<Product[]>('http://platzi-store.herokuapp.com/products');
   }
 
   // metodo para obtener un detalle de articulo en base al id
   getProduct(id: string) {
-    return this.products.find(item => id === item.id);
+    return this.http.get(`http://platzi-store.herokuapp.com/products${id}`);
   }
 
 }
--- src/app/product/components/products/products.component.ts
+++ src/app/product/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // se importa la interfaz Product
 import { Product } from './../../../core/models/product.model';
+import { ProductsService } from './../../../core/sevices/products/products.service';
 
 @Component({
   selector: 'app-products',
@@ -11,54 +12,16 @@ export class ProductsComponent implements OnInit {
 
   // array de productos
   // se tipa la array con la interfaz
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/bitcoin.jpg',
-      title: 'arroz',
-      price: 10,
-      description: 'x'
-    },
-    {
-      id: '2',
-      image: 'assets/images/gorillaz.jpg',
-      title: 'frijol',
-      price: 5,
-      description: 'x'
-    },
-    {
-      id: '3',
-      image: 'assets/images/responsive.jpg',
-      title: 'maiz',
-      price: 30,
-      description: 'x'
-    },
-    {
-      id: '4',
-      image: 'assets/images/one-more-time.jpg',
-      title: 'fresas',
-      price: 40,
-      description: 'x'
-    },
-    {
-      id: '5',
-      image: 'assets/images/rehuso.jpg',
-      title: 'naranja',
-      price: 10,
-      description: 'x'
-    },
-    {
-      id: '6',
-      image: 'assets/images/html5.jpg',
-      title: 'miel',
-      price: 50,
-      description: 'x'
-    },
-  ];
+  products: Product[] = [];
 
-  constructor() { }
+  // inyeccion de dependencias
+  constructor(
+    private productsService: ProductsService
+  ) { }
 
+  // peticion de la funcion fetchProducts
   ngOnInit() {
+    this.fetchProducts();
   }
 
   clickProduct(id: number) {
@@ -66,4 +29,11 @@ export class ProductsComponent implements OnInit {
     console.log(id);
   }
 
+  // funcion para obtener los productos
+  fetchProducts() {
+    this.productsService.getAllProducts()
+    .subscribe(products => {
+      this.products = products;
+    });
+  }
 }
