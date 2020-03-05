61b4a9d5e9cf37908fff582b0c5012ade9649c72
Bruno <brussdr7@gmail.com>
2020-03-05 13:52:24 -0600

    renderizacion de product detail y products
    la url para las peticiones se declara en los archivos enviroments y posteriormente se importa en los servicios del core.
    se presentaron errores 404 de las imagenes

--- src/app/core/sevices/products/products.service.ts
+++ src/app/core/sevices/products/products.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 
 import { Product } from './../../models/product.model';
 
+import { environment } from './../../../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,12 +18,12 @@ export class ProductsService {
   // obtener todos los productos
   getAllProducts() {
     // este get resuelve un array de tipo Product[]
-    return this.http.get<Product[]>('http://platzi-store.herokuapp.com/products');
+    return this.http.get<Product[]>(environment.url_api);
   }
 
   // metodo para obtener un detalle de articulo en base al id
   getProduct(id: string) {
-    return this.http.get(`http://platzi-store.herokuapp.com/products${id}`);
+    return this.http.get<Product>(`${environment.url_api}/${id}`);
   }
 
 }
