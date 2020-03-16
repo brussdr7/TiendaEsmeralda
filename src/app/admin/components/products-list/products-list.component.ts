dd01b380ace1742108ffc6a8be85a9425731c44d
Bruno <brussdr7@gmail.com>
2020-03-16 16:47:35 -0600

    Tabla de en modulo admin
    Se renderiza una nueva tabla dentro de del modulo admin para tener un un control en el inventario

--- src/app/admin/components/products-list/products-list.component.ts
+++ src/app/admin/components/products-list/products-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductsService } from './../../../core/sevices/products/products.service';
+
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  products = [];
+  displayedColumns: string[] = ['id', 'title', 'price', 'actions'];
 
+  constructor(
+    private productsService: ProductsService
+  ) { }
+
+  // ejecucion del metodo fetchProducts
   ngOnInit() {
+    this.fetchProducts();
+  }
+
+  // metodo que se encarga de traer todos los datos
+  fetchProducts() {
+    this.productsService.getAllProducts()
+    .subscribe(products => {
+      this.products = products;
+    });
+  }
+
+  deleteProduct(id: string) {
+    this.productsService.deleteProduct(id)
+    .subscribe(rta => {
+      this.fetchProducts();
+    });
   }
 
 }
