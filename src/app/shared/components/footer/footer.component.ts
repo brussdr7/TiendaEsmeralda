cda40f99f6cde940564e6e5d5f54ef19c5d215ac
Bruno <brussdr7@gmail.com>
2020-03-17 17:08:16 -0600

    Formulario reactivo
    Utilizando formularios reactivos podemos controlar de mejor manera los datos obtenidos

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  emailField: FormControl;
+
+  constructor() {
+    // se pueden agregar validaciones al form
+    // como un minimo y un maximo de caracteres
+    this.emailField = new FormControl('', [
+      Validators.required,
+      Validators.email
+      // Validators.minLength(4),
+      // Validators.maxLength(10)
+    ]);
+    // escuchar cambio dinamicamente
+    // this.emailField.valueChanges
+    // .subscribe(value => {
+    //   console.log(value);
+    // });
+  }
 
   ngOnInit() {
+  }
+
+  sendMail() {
+    if (this.emailField.valid) {
+      console.log(this.emailField.value);
+    }
   }
 
 }
