7f25a88ac8ecf26b06a8f25f716b8991b781ec47
Bruno <brussdr7@gmail.com>
2020-03-17 21:00:43 -0600

    Segundos pasos para Formulario desde Admin
    Se salvan los datos obtenidos del formulario para a partir de esos datos obetenidos crear un nuevo producto desde el modulo de admin.

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductsService } from './../../../core/sevices/products/products.service';
 
 @Component({
   selector: 'app-form-product',
@@ -11,7 +14,10 @@ export class FormProductComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    // Inyeccion de dependencias
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
   ) {
     this.buildForm();
   }
@@ -19,11 +25,31 @@ export class FormProductComponent implements OnInit {
   ngOnInit() {
   }
 
+  // Este metodo se encarga de recibir toda la informacion
+  // Se utiliza event para evitar que recargue la pagina
+  // al enviar el formulario
+  saveProduct(event: Event) {
+    event.preventDefault();
+    // se utiliza una condicional para continuar a crear producto
+    if (this.form.valid) {
+      // Se guarda el formulario en product
+      const product = this.form.value;
+      // Se crea el producto
+      this.productsService.createProduct(product)
+      .subscribe((newProduct) => {
+        console.log(newProduct);
+        // Redireccion desde nuestro controlador una vez creado
+        this.router.navigate(['./admin/products']);
+      });
+    }
+    console.log(this.form.value);
+  }
+
   private buildForm() {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
-      privateEncrypt: ['', [Validators.required]],
+      price: ['', [Validators.required]],
       image: [''],
       description: ['', [Validators.required]]
     });
